81733866423e10ebd19ef471cd4d2fd4f9c839e9
Kidmortal <kidmortal@gmail.com>
2021-09-18 14:44:37 -0300

    criado um belo front end responsivo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Troxa } from "./components/Troxa/Troxa";
+
+function App() {
+  return (
+    <div className="App">
+      <Troxa />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Troxa/Troxa.tsx
@@ -0,0 +1,5 @@
+import styles from "./Troxa.module.scss";
+
+export function Troxa() {
+  return <h1 className={styles.container}>troxa</h1>;
+}
