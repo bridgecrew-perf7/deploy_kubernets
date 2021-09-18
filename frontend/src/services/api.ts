5a2ecabe3b4dec40943e9efed38853540bce7c66
Kidmortal <kidmortal@gmail.com>
2021-09-18 16:13:35 -0300

    movido api url para env

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 class ApiClass {
   async _RequestApi(path: string) {
-    const response = await axios.get(`http://localhost:8000/${path}/`);
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_URL}/${path}/`
+    );
     return response.data;
   }
   async GetTodo() {
