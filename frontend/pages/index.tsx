9c716a30d6d97f80969b07078bed71bb158d2f88
Kidmortal <kidmortal@gmail.com>
2021-09-19 21:25:32 -0300

    mudanças front

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,22 +1,38 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+type HomeProps = {
+  response: any;
+};
+
+export default function Home({ response }: HomeProps) {
   const [server, setserver] = useState(false);
   useEffect(() => {
-    axios.get(`${process.env.API_URL}/todo`).then((r) => {
-      if (r?.data?.success) {
-        setserver(true);
-      }
-    });
+    axios
+      .get(`${process.env.API_URL}/todo`)
+      .then((r) => {
+        if (r?.data?.success) {
+          setserver(true);
+        }
+      })
+      .catch((er) => console.log(er));
   }, []);
   return (
     <div className={styles.container}>
-      <h1>O servidor está {server ? "Funcionando" : "Uma merda"}</h1>
+      <h1>Acesso via cliente {server ? "Funcionando" : "Uma merda"}</h1>
+      <h1>Acesso via servidor {response ? "Funcionando" : "Uma merda"}</h1>
     </div>
   );
-};
+}
 
-export default Home;
+export const getServerSideProps: GetServerSideProps = async function () {
+  const data = await axios.get(`${process.env.API_URL}/todo`);
+  const response = data.data;
+  return {
+    props: {
+      response,
+    },
+  };
+};
